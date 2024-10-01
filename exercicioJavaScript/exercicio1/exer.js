695d07f596ad2d36ff3e250f9a1a91242f474d17
ebuarbin <eduvardo16@gmail.com>
2024-10-01 12:44:42 -0300

    feito

---
+++ exercicioJavaScript/exercicio1/exer.js
@@ -0,0 +1,18 @@
+var arr1 = [1, 2, 3, 4, 5];
+var arr2 = [5, 6, 7, 8, 9, 10];
+
+function uniao(arr1, arr2) {
+    var arr3 = arr1.concat(arr2); 
+
+    for (let i = 0; i < arr3.length; i++) {
+        for (let j = i + 1; j < arr3.length; j++) { 
+            if (arr3[i] == arr3[j]) {
+                arr3.splice(j, 1);  
+                j--;  
+            }
+        }
+    }
+    return arr3;
+}
+
+console.log(uniao(arr1, arr2));
