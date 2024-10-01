5ce63d5c8f0193a23d09ed252540f0beb396b227
ebuarbin <eduvardo16@gmail.com>
2024-10-01 09:35:16 -0300

    ok

--- exercicioJavaScript/exercicio1/ex.js
+++ exercicioJavaScript/exercicio1/ex.js
@@ -5,11 +5,13 @@ var arr3 = []
 function interseccao(arr1, arr2){
     var arr3 = arr1.concat(arr2)
     
-    for (let i = 0; i < arr3.len; i++){
+    for (let i = 0; i < arr3.length; i++){
 
     }
     return arr3
 } 
+
+
 
 
 console.log(interseccao(arr1, arr2))
