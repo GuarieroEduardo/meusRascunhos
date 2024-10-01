a249ce216158c3c37a4d671f245cb6db3c044759
ebuarbin <eduvardo16@gmail.com>
2024-10-01 08:59:10 -0300

    feito

--- exercicioJavaScript/exercicio1/ex.js
+++ exercicioJavaScript/exercicio1/ex.js
@@ -1,21 +1,11 @@
-var arr1 = [1, 2, 3, 4 , 5]
+var arr1 = [1, 2, 3, 4 , 5,]
 var arr2 = [5, 6, 7 ,8, 9, 10]
 var arr3 = []
 
 function interseccao(arr1, arr2){
-    var arr3 = []
-    for (let i = 0; i < arr1.length; i++){
-        if (arr1[i] in arr3){
-            arr3.push(arr1[i])
-        }
-
-    }
-    for (let j = 0; j < arr2.length; j++){
-        if (arr2[j] in arr3){
-            arr3.push(arr2[j])
-        }
-
-    }
+    var arr3 = arr1.concat(arr2)
+    
+    for (let i = 0; i < arr3.)
     return arr3
 } 
 
