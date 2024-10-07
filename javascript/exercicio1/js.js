109ff77dedef4a75cb43f46e80eb2781fcef6c20
ebuarbin <eduvardo16@gmail.com>
2024-10-07 08:08:28 -0300

    feito

--- javascript/exercicio1/js.js
+++ javascript/exercicio1/js.js
@@ -2,18 +2,16 @@ var div = document.getElementById("meuId")
 var botao = document.getElementsByClassName('Button')
 var text = document.createElement("div")
 
-
 var mostrar = 0
-if (mostrar == 0){
-    botao[0].addEventListener('click', function(){
+
+botao[0].addEventListener('click', function(){
+    if (mostrar == 0){
         text.innerHTML = "<p>eu tentei</p>"
         div.appendChild(text)
-        mostrar +=1
-    })
-}
-if (mostrar == 1){
-    botao[0].addEventListener('click', function(){
-        div.remove(text)
-        mostrar -=1
-    })
-}
+        mostrar = 1
+    }else{
+        text.remove()
+        mostrar = 0
+    }
+
+})
