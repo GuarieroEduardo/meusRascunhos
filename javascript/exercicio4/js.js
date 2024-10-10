e86c5969a5981c5bf044ad7e0edd206f75c5430b
ebuarbin <eduvardo16@gmail.com>
2024-10-09 21:24:08 -0400

    feito

--- javascript/exercicio4/js.js
+++ javascript/exercicio4/js.js
@@ -2,17 +2,29 @@ var painel = document.getElementById("menu")
 var botao = document.getElementsByClassName("add")
 var rev = document.getElementsByClassName("rmv")
 var cont = 1
+var x = 0
+
 
 botao[0].addEventListener("click", function(){
-    var entrada = document.getElementsByClassName("entrada")
-    var item = document.createElement("div")
-    item.innerHTML = `<div>item ${cont}: ${entrada.value} <button class='rmv'>remover</button>`
-    painel.appendChild(item)    
-    cont+=1
+    var entrada = document.getElementById("entrada")
+    console.log(entrada.value)
+    if (entrada.value != ""){
+        var item = document.createElement("div")
+        item.innerHTML = `<p><ul><li>item: ${cont}: ${entrada.value} <button class='rmv'>remover</button></li></ul></p>`
+        painel.appendChild(item)    
+        cont+=1
+    }
+    
+
+    console.log(rev)
+    rev[x].addEventListener("click", function(){
+        painel.removeChild(item)
+        x = x - 1
+        
+    })
+    x +=1
     
 
 })
 
-rev.addEventListener("click", function(){
-    item.remove()
-})
+
