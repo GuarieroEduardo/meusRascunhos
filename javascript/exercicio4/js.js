39f2d6b9992a80471e1dbcd235b922705143d214
ebuarbin <eduvardo16@gmail.com>
2024-10-10 10:51:04 -0300

    ok

--- javascript/exercicio4/js.js
+++ javascript/exercicio4/js.js
@@ -1,30 +1,55 @@
-var painel = document.getElementById("menu")
-var botao = document.getElementsByClassName("add")
-var rev = document.getElementsByClassName("rmv")
-var cont = 1
-var x = 0
+var painel = document.getElementById("menu");
+var botao = document.getElementsByClassName("add")[0];
+var cont = 1;
 
+botao.addEventListener("click", function () {
+    var entrada = document.getElementById("entrada");
+    if (entrada.value !== "") {
+        var item = document.createElement("li");
 
-botao[0].addEventListener("click", function(){
-    var entrada = document.getElementById("entrada")
-    console.log(entrada.value)
-    if (entrada.value != ""){
-        var item = document.createElement("div")
-        item.innerHTML = `<p><ul><li>item: ${cont}: ${entrada.value} <button class='rmv'>remover</button></li></ul></p>`
-        painel.appendChild(item)    
-        cont+=1
+        var textoItem = document.createElement("span");
+
+        textoItem.textContent = `item ${cont}: ${entrada.value} `;
+
+        var botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "Remover";
+        botaoRemover.className = 'rmv';
+
+        botaoRemover.addEventListener("click", function () {
+            item.remove();
+            ordenarItens();
+        });
+
+        item.appendChild(textoItem);
+        item.appendChild(botaoRemover);
+
+        var lista = painel.querySelector("ul");
+        if (!lista) {
+            lista = document.createElement("ul");
+            painel.appendChild(lista);
+        }
+
+        lista.appendChild(item);
+
+        cont += 1;
+        entrada.value = "";
     }
-    
+});
 
-    console.log(rev)
-    rev[x].addEventListener("click", function(){
-        painel.removeChild(item)
-        x = x - 1
-        
-    })
-    x +=1
-    
+function ordenarItens() {
+    var itens = painel.querySelectorAll("li");
+    cont = 1;
 
-})
+    for (var i = 0; i < itens.length; i++) {
+        var item = itens[i];
+        var textoItem = item.querySelector("span");
 
+        var partes = textoItem.textContent.split(":");
 
+        var nomeItem = partes[1].trim();
+
+        textoItem.textContent = `item ${cont}: ${nomeItem}`;
+
+        cont += 1;
+    }
+}
